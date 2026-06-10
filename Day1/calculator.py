2ac29b6443da7eaa00657ead9a70e4e71d2e8955
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-10 11:27:57 +0530

    Update calculator.py

--- Day1/calculator.py
+++ Day1/calculator.py
@@ -1,1 +1,15 @@
+a=float(input('enter the first number'))
+b=float(input('enter the second number'))
+r1=a+b
+print(r1)
+r2=a-b
+print(r2)
+r3=a*b
+print(r3)
+if b!=0:
+    r4=a/b
+    print(r4)
+else:
+    print('division is not possible')
+
 
