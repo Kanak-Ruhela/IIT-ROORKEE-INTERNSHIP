f7753ed25ece34ab33f5293c07f55c981fd41c61
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-10 16:06:38 +0530

    Update Grades.py

--- Day2/Grades.py
+++ Day2/Grades.py
@@ -1,1 +1,11 @@
-
+a=int(input('Enter the number:-'))
+if a>90:
+    print('Grade A')
+elif a<90 and a>80:
+    print('Grade B')
+elif a < 80 and a >70:
+    print('Grade C')
+elif a<70 and a>60:
+    print('Grade D')
+else:
+    print('Fail')
