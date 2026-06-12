21aaf1227053e842359f6098e1ff8361d8af5413
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-12 14:35:10 +0530

    Update FizzBuzz.py

--- Day2/FizzBuzz.py
+++ Day2/FizzBuzz.py
@@ -1,1 +1,10 @@
-
+a=int(input('Enter the number:-'))
+for a in range(1,11):
+    if a%3==0 and a%5==0:
+        print('FizzBuzz')
+    elif a%3==0:
+        print('Fizz')
+    elif a%5==0:
+        print('Buzz')
+    else:
+        print(a)
