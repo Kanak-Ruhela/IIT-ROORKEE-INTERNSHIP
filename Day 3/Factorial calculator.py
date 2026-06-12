a6a0a97bfde846c2bab7f0e11694655462c3a6d4
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-12 17:01:22 +0530

    Update Factorial calculator.py

--- Day 3/Factorial calculator.py
+++ Day 3/Factorial calculator.py
@@ -1,1 +1,13 @@
-
+def factorial_iterative(n):
+    fact=1
+    for i in range (1,n+1):
+        fact = fact*i
+    return fact
+print(factorial_iterative(int(input('enter the number:-'))))
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
+print(factorial_recursive(int(input('enter the number:-'))))
+   
