516f8b047d73b2f88da69a486a50da4e167fcbc4
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-12 16:38:01 +0530

    Update Password generator.py

--- Day 3/Password generator.py
+++ Day 3/Password generator.py
@@ -1,1 +1,7 @@
-
+import random
+import secrets
+import string
+chars = string.ascii_letters + string.punctuation + string.digits
+password = ''.join(secrets.choice(chars)
+for _ in range(int(input('enter the range'))))
+print(password)
