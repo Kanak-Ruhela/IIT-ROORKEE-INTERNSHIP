17180a99a7af2d3fda44989c53081a083abf2427
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-12 14:39:32 +0530

    Implement palindrome check in Palindrome.py

--- Day 3/Palindrome.py
+++ Day 3/Palindrome.py
@@ -1,1 +1,10 @@
-
+n=int(input('Enter the number:-'))
+rev=0
+original=n
+while (n>0):
+    rev=(rev*10)+n%10
+    n=n//10
+if original==rev: 
+    print("Palindrome")
+else:
+    print("Not palindrome")
