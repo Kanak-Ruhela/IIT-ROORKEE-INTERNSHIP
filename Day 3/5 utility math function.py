2190faaa2749828ce3d1e44699e0a6a29c2478f8
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-12 14:43:09 +0530

    Update 5 utility math function.py

--- Day 3/5 utility math function.py
+++ Day 3/5 utility math function.py
@@ -1,1 +1,26 @@
+def add(a,b):
+    add=a+b
+    print(add)
+    return add
+def substract(a,b):
+    sub=a-b
+    print(sub)
+    return sub
+def multiply(a,b):
+    multiply=a*b
+    print(multiply)
+    return multiply
+def divide(a,b):
+    divide=a/b
+    print(divide)
+    return divide
+def square(a):
+    sqrt=a*a
+    print(sqrt)
+    return sqrt
+add(2,2)
+substract(4,2)
+multiply(2,2)
+divide(4,2)
+square(2)
 
