dd5768647b18eb0fca13776d41e615b9b617143a
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-13 21:34:16 +0530

    Update Student Grade Tracker.py

--- Day 4/Student Grade Tracker.py
+++ Day 4/Student Grade Tracker.py
@@ -1,1 +1,11 @@
+A = ("Rahul", 85)
+B = ("Anjali", 92)
+C = ("Amit", 78)
+D = ("Priya", 95)
+print(A[0],'=' ,A[1])
+print(B[0],'=', B[1])
+print(C[0],'=', C[1])
+print(D[0],'=', D[1])
+average = (A[1] + B[1] + C[1] + D[1]) / 4
+print("Average:", average)
 
