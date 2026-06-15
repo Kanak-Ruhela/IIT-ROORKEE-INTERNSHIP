80ba407da1bc5728ac9f57d1deb32f7d6d0ea7d4
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-15 10:51:00 +0530

    Update Phonebook.py

--- Day 5/Phonebook.py
+++ Day 5/Phonebook.py
@@ -1,1 +1,6 @@
-
+phonebook = {"Father":"8946583857","Mother":"3757362940"}
+new_phonebook={"Brother":"5674836577"}
+phonebook.update(new_phonebook)
+print(phonebook.get("Father"))
+del phonebook["Mother"]
+print(phonebook)
