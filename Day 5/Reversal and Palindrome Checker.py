1b0b1362e51da25aca147f1c909066967a7dad18
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-15 12:26:59 +0530

    Update Reversal and Palindrome Checker.py

--- Day 5/Reversal and Palindrome Checker.py
+++ Day 5/Reversal and Palindrome Checker.py
@@ -1,1 +1,7 @@
-
+def check_palindrome(word):
+    if word == word[::-1]:
+        print("Yes, it is a palindrome!")
+    else:
+        print("No, it is not a palindrome.")
+a=input('enter the word:-')
+check_palindrome(a)
