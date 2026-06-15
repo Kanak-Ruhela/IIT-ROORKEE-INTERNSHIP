e2229de7044f1a1ff4ef1bef8cd0628407bc9d62
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-15 12:38:56 +0530

    Update Anagram checker.py

--- Day 5/Anagram checker.py
+++ Day 5/Anagram checker.py
@@ -1,1 +1,8 @@
-
+def check_anagram(word1, word2):
+    if sorted(word1)==sorted(word2):
+        print("word is anagrom")
+    else:
+        print("word is not anagram")
+a= input("enter the first letter:-")
+b=input("enter the second letter:-")
+check_anagram(a,b)
