9c890d649b36fd1c7cba56a4725d7fec74ec6db6
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-22 10:14:10 +0530

    Update division function.py

--- Day 6/division function.py
+++ Day 6/division function.py
@@ -1,1 +1,6 @@
-
+def safe_divide_simple(numerator, denominator):
+    if denominator == 0:
+        return 0 
+    return numerator / denominator
+print(safe_divide_simple(12, 3)) 
+print(safe_divide_simple(12, 0))  
