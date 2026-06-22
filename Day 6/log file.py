fe94e7d8382f1bdd8e8942c5cbe342bbe92d25df
Kanak-Ruhela <ruhelakanak52@gmail.com>
2026-06-22 10:21:07 +0530

    Update log file.py

--- Day 6/log file.py
+++ Day 6/log file.py
@@ -1,1 +1,21 @@
+error_counts = {}
+try:
+    with open("server.log", "r") as file:
+        for line in file:
+            if "ERROR" in line:
+                parts = line.split("ERROR:")
+                if len(parts) > 1:
+                    error_type = parts[1].strip().split()[0]
+                    if error_type in error_counts:
+                        error_counts[error_type] += 1
+                    else:
+                        error_counts[error_type] = 1
+    print("\n--- Log Analysis Report ---")
+    if not error_counts:
+        print("No errors found in the log file!")
+    else:
+        for error, count in error_counts.items():
+            print(f"{error}: {count} times")
+except FileNotFoundError:
+    print("Error: The log file 'server.log' was not found.")
 
